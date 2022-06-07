4d028e3ed8603d495fe0d16494051ee43d922c98
purplecactuszt <59769757+purplecactuszt@users.noreply.github.com>
2022-06-08 00:01:22 +0200

    Add files via upload

---
+++ exension/main.js
@@ -0,0 +1,7 @@
+let will = Math.floor(Math.random() * 101);
+var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+if (will == 1){
+    if (window.location != url){
+        window.location = url;
+    }
+}
